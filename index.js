2b7b8f65ac098a954627867b7714d5766d5f2d1c
SudhamDebnath <sudham15-12854@diu.edu.bd>
2023-06-08 18:31:12 +0600

    Initial project server setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("Shutter Skills School Server is running successfully on port 5000");
+  });
+  
+  app.listen(port, () => {
+    console.log(`Shutter Skills School Server is running on port : ${port}`);
+  });
